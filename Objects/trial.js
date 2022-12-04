36218b5e0109abbbbc446f563f85f19dbb0588e0
Titus Kiplagat <tituskiplagat210@gmail.com>
2022-12-04 21:40:46 +0300

    secluded each project to its file

--- Objects/trial.js
+++ Objects/trial.js
@@ -1,23 +1,27 @@
-// creating functionasl object
 
 
-totalUSers = Number(prompt("Enter the number of users:"))
-function createEmployee(name, status, salary) {
+// multiply numeric values by two
+// before call
+let user = {
+    Name: 'John',
+    Math: 80,
+    English: 85,
+    French: 99
+}
+alert(`BEfore multiplying numeric \n ${JSON.stringify(user)}`)
 
-    return (
-        employee = {
-            name: name,
-            status: status,
-            salary: salary
-        }
-    )
+// create function to multiply numeric values by two
+
+
+
+function multiplyNumber(user) {
+    for (let value in user) {
+        if (typeof (user[value]) === 'number')
+            user[value] *= 2;
+    }
 
 }
 
-for (i = 0; i < totalUSers; i++) {
-
-    empName = prompt("Enter employee name")
-    empStatus = prompt("Enter employee status")
-    empSalary = prompt("Enter employee salary")
-    createEmployee(empName, empStatus, empSalary)
-}
+// after multiplying numeric values
+multiplyNumber(user)
+alert(`After multiplying numeric \n ${JSON.stringify(user)}`)
