981af77a14e8b296ed9a0db9da384adffea74058
Titus Kiplagat <tituskiplagat210@gmail.com>
2023-01-15 14:28:31 +0300

    Linear Big-0

---
+++ DataStructures/Big-0/linear.js
@@ -0,0 +1,12 @@
+/* Linear Big-0 implementation */
+
+// array initialization and declaration
+var array = [1,2,3,4,5,6,7,8,9,10]
+var size = array.length
+
+console.log(size)
+
+// loop through the array and print
+for( let i = 0; i < size; i++){
+    console.log(`The array at index ${i} is ${array[i]}`)
+}
