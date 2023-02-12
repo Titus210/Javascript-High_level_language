da1abde5fad9d523ad94c02fb0ac0089f04bdc55
Titus Kiplagat <tituskiplagat210@gmail.com>
2023-02-12 22:57:20 +0300

    Pyhton code

--- Duckeet Lessons/Events/checkUSername/username.js
+++ Duckeet Lessons/Events/checkUSername/username.js
@@ -1,16 +1,7 @@
 
-function checkUsername() {
-
-
-    // check username length
-    if (userName.value.length < 5){
-        errorMessage.textContent = "Username Must be 5 characters or More";
-        userName.style.backgroundColor = 'rgba(255, 255, 23)';
-}
-    else {
-        errorMessage.textContent = '';
-    }
-}
-
 var errorMessage = document.getElementById('feedback');
 var userName = document.getElementById('username');
+
+errorMessage.addEventListener('blur', function () {
+    alert("Sotty")
+}, false)
