821abc52f3d468a350e7a93b24dbc2807bdfd3bb
Titus Kiplagat <tituskiplagat210@gmail.com>
2023-03-31 16:31:29 +0300

    Restaurant Object

---
+++ 100+_Js_Projects/Restaurant Calculator/restaurantObject.js
@@ -0,0 +1,45 @@
+// array to hold all the bills
+var bills = [124, 48, 268, 180, 42];
+
+
+
+var bill_tips = [];
+var tips = [];
+var hotels = ["Soodvalley", "Santuri", "Eatify", "foodie", "yummy"];
+var tip = 0;
+
+// John Object
+var john = {
+    bills: [124, 48, 268, 180, 42],
+    tipCalc: function () {
+
+        let tip = 0;
+        for (var i = 0; i <= this.bills.length; i++) {
+            if (this.bills[i] <= 50) {
+                tip = 0.2 * this.bills[i];
+
+            }
+            else if (this.bills[i] > 50 && bills[i] <= 200) {
+                tip = 0.15 * this.bills[i];
+            }
+            else if (this.bills[i] > 200) {
+                tip = 0.1 * this.bills[i];
+            }
+            else
+                return tip;
+            tips.push(tip.toFixed(2));
+        }
+
+    }
+}
+
+john.tipCalc();
+console.log(tips);
+
+//loop through hotel and make a new list for each hotel
+for (var i = 0; i < hotels.length; i++) {
+   let bill_tip = hotels[i] + ": tip " + `${tips[i]}` + " " + ", bill: " + `${john.bills[i]}`;
+    bill_tips.push(bill_tip);
+}
+
+console.log(bill_tips);
