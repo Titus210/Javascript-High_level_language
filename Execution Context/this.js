7f0fb3b8be88acaa8044bea0b542ded6b8c0bd74
Titus Kiplagat <tituskiplagat210@gmail.com>
2023-04-01 20:54:03 +0300

    this keyword

--- Execution Context/this.js
+++ Execution Context/this.js
@@ -1,6 +1,6 @@
 console.log(this) // refers to global object
 
-function calculateAge(year){
+function calculateAge(year) {
     console.log(2023 - year)
     console.log(this) // refers to global object
 }
@@ -8,13 +8,27 @@ function calculateAge(year){
 calculateAge(2002)
 
 
-var john ={
+var john = {
     name: 'John',
     yob: 2002,
-    calculateAge: function(){
+    calculateAge: function () {
         console.log(2023 - this.yob)
-        console.log(this) // refers to john object
-    
+       //  console.log(this) // refers to john object
+        function innerFunct() {
+          //  console.log(this) // window global object
+        }
+        innerFunct()
     }
 }
 john.calculateAge()
+
+
+var mike ={
+    name : 'mike',
+    yob : 2000,
+   
+}
+
+// borrows from john object
+mike.calculateAge = john.calculateAge;
+mike.calculateAge();
