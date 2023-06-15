f0d1c1a578e34ffdf915bbf2a6a904122c7afe0c
Titus210 <tituskiplagat210@gmail.com>
2023-06-16 00:47:34 +0300

    matches items that are same as the one searched - dsnt return one unlike find

---
+++ Arrays/5-filter.js
@@ -0,0 +1,10 @@
+let users=[
+    {name:"john", car:"BMW"},
+    {name:"mark", car:"volvo"},
+    {name:"phill", car:"audi"},
+    {name:"japheth", car:"BMW"},
+]
+
+let user = users.filter(item=>item.car == 'BMW');
+
+console.log(user)
