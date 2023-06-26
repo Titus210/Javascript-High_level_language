37cfbf98ea67ac99d006f3137b80f61f8f3a7566
Titus210 <tituskiplagat210@gmail.com>
2023-06-26 19:58:38 +0300

    pyrhagoras tgheorem using Math function

---
+++ Introduction/intro.js
@@ -0,0 +1,23 @@
+
+var admin;
+var user_name;
+
+user_name = 'John';
+admin= user_name;
+
+console.log(admin)
+
+var earth;
+var latest_visitor;
+
+
+let age = 12
+
+console.log(typeof(age))    // number
+console.log(typeof("foo")) // string
+console.log(typeof(null))
+
+let string_number ="123"
+console.log(typeof string_number)
+let _num = Number(string_number)
+console.log(typeof _num)
