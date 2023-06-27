8dbaf85cfb40c259e3aaaf04b06b780e92fcd9a9
Titus210 <tituskiplagat210@gmail.com>
2023-06-27 23:06:22 +0300

    checking users age - using ternary operator

---
+++ Introduction/1-age.js
@@ -0,0 +1,9 @@
+let age = 12;
+
+if (age >= 18) {
+  console.log("Youre an adult");
+} else {
+  console.log("Youre a child");
+}
+let age_2 = 21;
+age_2 >= 18 ? console.log("Youre an adult") : console.log("Youre a child");
